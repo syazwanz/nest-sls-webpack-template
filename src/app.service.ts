6e945a0e235cd4be95ec4a18241ef3f04c86d70a
syazwanz <syazwanz.dev@gmail.com>
2024-04-05 15:39:30 +0800

    add argon2

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import argon2 from 'argon2';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    const hash = await argon2.hash('password');
+
+    return `Hello World! ${hash}`;
   }
 }
