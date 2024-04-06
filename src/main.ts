e77f6b9b08f49878aaa0bd8466932d6f572e922c
syazwanz <syazwanz.dev@gmail.com>
2024-04-06 14:55:43 +0800

    integrate serverless app inside main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+
+let server: Handler;
+
+async function bootstrapSLS(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrapSLS());
+  return server(event, context, callback);
+};
+
+if (process.env.NEST_ENV === 'HTTP') {
+  bootstrap();
+}
